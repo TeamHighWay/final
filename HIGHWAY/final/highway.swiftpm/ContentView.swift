1334e9bd505da5c3f582b21548a272a79af4ec28
Tyler <hyunjuntyler@gmail.com>
2023-03-30 11:39:42 +0900

    how to use 로 변경

--- HIGHWAY/final/highway.swiftpm/ContentView.swift
+++ HIGHWAY/final/highway.swiftpm/ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!2")
+            Text("how to use")
         }
     }
 }
